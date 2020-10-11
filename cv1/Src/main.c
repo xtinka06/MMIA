549f22cf653e9346f1e45663246f3b5622831685
xtinka06@vutbr.cz <xtinka06@vutbr.cz>
2020-10-11 16:10:37 +0200

    cv1 SOS

--- cv1/Src/main.c
+++ cv1/Src/main.c
@@ -16,23 +16,31 @@
  *
  ******************************************************************************
  */
+#include "stm32f0xx.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-#include "stm32f0xx.h"
+
 
 int main(void)
 {
 		RCC->AHBENR |= RCC_AHBENR_GPIOAEN;				//Enable the peripheral clock of GPIOA
 		GPIOA->MODER|=GPIO_MODER_MODER5_0;				//Select output mode (01) on GPIOA pin 2
+		static const uint8_t pole[32]={1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
 
     /* Loop forever */
 	for(;;)
 	{
-		GPIOA->ODR ^= (1<<5); // toggle
-		for (volatile uint32_t i = 0; i < 100000; i++) {}
+		for(volatile uint8_t i = 0; i < 32; i++) {					//nastavení výstupu na 1 nebo 0
+					if (pole[i]==1)
+							GPIOA->BSRR = (1<<5);
+						else
+							GPIOA->BRR = (1<<5);
+					}
+
+				for (volatile uint32_t i = 0; i < 100000; i++) {}
 
 	}
 }
