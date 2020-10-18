2d4df561ab0569b9dc4eafa2f394b93792868a30
xtinka06@vutbr.cz <xtinka06@vutbr.cz>
2020-10-18 20:15:57 +0200

    cv2_SysTick

--- cv2/Src/main.c
+++ cv2/Src/main.c
@@ -35,6 +35,22 @@ void EXTI0_1_IRQHandler(void)
 	}
  }
 
+void SysTick_Handler(void)
+ {
+ Tick++;
+ }
+
+
+void blikac(void)
+ {
+	static uint32_t delay;
+	if (Tick > delay + LED_TIME_BLINK) {
+		GPIOA->ODR ^= (1<<4) ;
+		delay = Tick;
+	}
+ }
+
+
 
 int main(void)
 {
@@ -47,15 +63,16 @@ int main(void)
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
 
 	SysTick_Config(8000); // 1ms
-
+/*
 	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
 	EXTI->IMR |= EXTI_IMR_MR0; // mask
 	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
 	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
-
+*/
 
     /* Loop forever */
 	for(;;) {
+		blikac();
 
 	}
 }
